eca5a179850def9de186fcc0514db1f65e5e57cf
4zu1Q <135081843+4zu1Q@users.noreply.github.com>
2024-12-25 20:22:46 +0900

    up

--- SceneTemplate22/Main.cpp
+++ SceneTemplate22/Main.cpp
@@ -26,7 +26,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// 一部の関数はDxLib_Init()の前に実行する必要がある
 	//ウィンドウモードの設定
 #ifdef _DEBUG
-	ChangeWindowMode(false);
+	ChangeWindowMode(true);
 #else
 	ChangeWindowMode(false);
 #endif
